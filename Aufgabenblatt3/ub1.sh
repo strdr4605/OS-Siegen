751f7dbef20ad80e6f56402c384e1cc5745a6b8f
strdr4605 <str.dr4605@gmail.com>
2017-11-02 16:57:58 +0100

    Add 3rd exercise and solve 1st and 2nd task

---
+++ Aufgabenblatt3/ub1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LOCATION=$1
+FILECOUNT=0
+DIRCOUNT=0
+
+if [ "$#" -lt "1" ]
+then
+    echo "Usage: ./test2.sh <directory>"
+    exit 0
+fi
+
+for item in $LOCATION/*
+do
+if [ -f "$item" ]
+    then
+         FILECOUNT=$[$FILECOUNT+1]
+    elif [ -d "$item" ]
+        then
+         DIRCOUNT=$[$DIRCOUNT+1]
+fi
+done
+
+echo "File count: " $FILECOUNT
+echo "Directory count: " $DIRCOUNT
---
+++ Aufgabenblatt3/ub2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DATE=$(date +"<%m.%d.%Y>")
+TIME=$(date +"<%H:%M>")
+
+echo "Today is $DATE" 
+echo "Current time: $TIME"
