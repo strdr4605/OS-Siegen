21e0de11ff5aeb91a31f1fa37f98d312afa190ce
sspatari <spatari.stanislav@gmail.com>
2017-11-03 13:26:48 +0100

    Solve bug related to -s option finding not only files but also directories that are empty

--- lab3/countFiles.sh
+++ lab3/countFiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for file in "${1-.}"/*; do
         [ -d "${file}" ] && ((directories++))
-        [ -s "${file}" ] && ((nonEmptyFiles++))
+        [ -s "${file}" ] && [ -f "${file}" ]&& ((nonEmptyFiles++))
         [ -f "${file}" ] && ((files++))
 done
 
 echo "Number of directories: ${directories}"
 echo "Number of non empty files: ${nonEmptyFiles}"
 echo "Number of empty files: $((files - nonEmptyFiles))"
-
+echo "${1-.}"/*
